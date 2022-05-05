ee65dd7a38b4da22caa4b2ce44d7b8c3e241dc9f
lemonclouds <hannah.lakhai@gmail.com>
2022-05-05 22:07:14 +0300

    Add EntityAlreadyExistsException and ExceptionMessage

---
+++ src/main/java/com/company/financeApp/constants/ExceptionMessage.java
@@ -0,0 +1,7 @@
+package com.company.financeApp.constants;
+
+public class ExceptionMessage {
+    public static final String USER_WITH_ID_NOT_FOUND = "User[%d] not found";
+    public static final String CATEGORY_WITH_ID_NOT_FOUND = "Category[%d] not found";
+    public static final String TRANSACTION_WITH_ID_NOT_FOUND = "Transaction[%d] not found";
+}
